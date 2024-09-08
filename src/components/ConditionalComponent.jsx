52e527b75525ad9237e45bfb32e4b6c19f1084a9
nhiendev <nhiennkrb@gmail.com>
2024-09-08 17:42:42 +0700

    deploy Github pages

---
+++ src/components/ConditionalComponent.jsx
@@ -0,0 +1,7 @@
+export default function ConditionalComponent(){
+    const display = true;
+    return <div>
+        <h3>This is a Conditional Component</h3>
+        <h3>Code everyday</h3>
+    </div>;
+}
